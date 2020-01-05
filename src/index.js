448a1665253de2a38006f2f8979a115372156553
OPP Developer <developer.opp@gmail.com>
2020-01-05 14:17:01 +0400

    App Configured for IIS

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import App from "./App";
 // CSS Files
 import "@material/react-material-icon/dist/material-icon.css";
 
-const ENV =
-  process.env.NODE_ENV === "development" ? (
-    <Router>
-      <App />
-    </Router>
-  ) : (
-    <Router basename={window.location.pathname || ""}>
-      <App />
-    </Router>
-  );
+// const ENV =
+//   process.env.NODE_ENV === "development" ? (
+//     <Router>
+//       <App />
+//     </Router>
+//   ) : (
+//     <Router basename={window.location.pathname || ""}>
+//       <App />
+//     </Router>
+//   );
 
-ReactDOM.render(ENV, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
